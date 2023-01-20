fa39a921e08662384a5ea8e8ba52b198b2102328
edouard <edouard.gugenheim@gmail.com>
2023-01-20 16:13:49 +0100

    whatyearisit

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/year', (req, res) => {
- const date = new Date();
+ const date = new Date().getFullYear();
  res.json({ year: date });
 });
 
